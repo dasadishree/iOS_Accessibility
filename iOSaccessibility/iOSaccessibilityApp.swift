71bd67202c2f5ae2beb53d667766250680ed7eba
Adishree Das <dasadishree@gmail.com>
2025-08-10 09:34:50 -0400

    created xcode project

---
+++ iOSaccessibility/iOSaccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSaccessibilityApp.swift
+//  iOSaccessibility
+//
+//  Created by Adishree Das on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct iOSaccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
